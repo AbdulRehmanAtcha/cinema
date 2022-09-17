c9852fcb14a1cbb09c33782f93f06e983cb54fa5
Abdul rehman <92687624+AbdulRehmanAtcha@users.noreply.github.com>
2022-09-17 20:01:12 +0500

    Add files via upload

---
+++ cinema/app.js
@@ -0,0 +1,7 @@
+// Initializng Variables
+var signUpBar = document.querySelector(".signup-bar");
+var signInBar = document.querySelector(".signin-bar");
+
+// Initiailizing default display.
+signUpBar.className += " active";
+signInBar.className += " non-active";
